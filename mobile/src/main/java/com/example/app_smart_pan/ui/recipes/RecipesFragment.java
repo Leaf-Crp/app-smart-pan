a1101cf9257dc9758778dc8035c0eb67556a5ee0
melassouri <mohammed.elassouri@sqits.net>
2020-03-22 22:24:14 +0100

    Recipes Page

---
+++ mobile/src/main/java/com/example/app_smart_pan/ui/recipes/RecipesFragment.java
@@ -0,0 +1,25 @@
+package com.example.app_smart_pan.ui.recipes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.app_smart_pan.R;
+
+public class RecipesFragment extends Fragment {
+
+    private RecipesViewModel recipesViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        recipesViewModel =
+                ViewModelProviders.of(this).get(RecipesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_recipes, container, false);
+        return root;
+    }
+}
