70d64000abed40bf8ddf4eb09de62f1356fdb1d2
melassouri <mohammed.elassouri@sqits.net>
2020-03-23 11:10:13 +0100

    Recipes Page

--- mobile/src/main/java/com/example/app_smart_pan/ui/profile/ProfileFragment.java
+++ mobile/src/main/java/com/example/app_smart_pan/ui/profile/ProfileFragment.java
@@ -15,10 +15,8 @@ public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         return root;
     }
