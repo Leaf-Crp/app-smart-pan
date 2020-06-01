d571d90f56dfc6f51d66367baaa84d2e27982d7d
Mohammed EL ASSOURI AKA Quick Fix <mohammed.elassouri@sqits.net>
2020-06-01 18:21:24 +0200

    Session Part 1

--- services/src/main/java/com/example/services/api/config/Config.java
+++ services/src/main/java/com/example/services/api/config/Config.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Config {
 
-    private final static String BASE_URL = "http://192.168.1.29:3001/";
+    private final static String BASE_URL = "http://192.168.0.16:3001/";
 
     private static Retrofit RETROFIT = new Retrofit.Builder()
             .baseUrl(BASE_URL)
